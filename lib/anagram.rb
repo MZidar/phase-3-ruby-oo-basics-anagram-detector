129f770e10b8eadcbd0fad8c9b2ce7de406c2a69
MZidar <MZidar@gmail.com>
2021-09-27 10:21:27 -0400

    commit tree 75db8379447a3a25263d3ade04c5cd417b31e1c4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :string, :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        final = []
+        string.each do |ana|
+            if ana.chars.sort == word.chars.sort
+            final << ana
+
+            end
+        end
+        return final
+
+    end
+
+    
+
+end
+
+binding.pry
